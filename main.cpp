482923394a4419a723d48b8e3fde47b0e8a298ee
Abdallah Abdelaziz <abdallah_taha@aucegypt.edu>
2023-11-03 20:51:58 +0200

    getAverage implemented and tested (no support for empty vectors)

--- main.cpp
+++ main.cpp
@@ -11,9 +11,21 @@ int getSum(vector<int> arr) {
     return sum;
 }
 
+float getAverage(vector<int> arr) {
+    float sum = 0;
+    for (auto& element : arr) {
+        sum += element;
+    }
+    return sum / arr.size();
+}
+
 int main() {    
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     cout << "The sum of the vector {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} is "
          << getSum(arr) << ".\n";
+
+    cout << "The average of the vector {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} is "
+         << getAverage(arr) << ".\n";
+         
     return 0;
 }
