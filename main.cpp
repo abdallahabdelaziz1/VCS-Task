16a8e6372a09efdbdd0b65b34771affbfa6c741b
Abdallah Abdelaziz <abdallah_taha@aucegypt.edu>
2023-11-03 20:56:26 +0200

    getMin implemented and tested (no support for empty vectors)

--- main.cpp
+++ main.cpp
@@ -11,9 +11,23 @@ int getSum(vector<int> arr) {
     return sum;
 }
 
+int getMin(vector<int> arr) {
+    int min = arr[0];
+    for (auto& element : arr) {
+        if (element < min) {
+            min = element;
+        }
+    }
+    return min;
+}
+
 int main() {    
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     cout << "The sum of the vector {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} is "
          << getSum(arr) << ".\n";
+
+    cout << "The min of the vector {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} is "
+         << getMin(arr) << ".\n";
+    
     return 0;
 }
